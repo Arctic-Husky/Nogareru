fa6d0a5ef831e0a5218f3e87d60f49e05d0e4115
Caio Schmidt <caiofonseca6@gmail.com>
2021-09-29 21:01:53 -0300

    Adicionado InputNomeJogador

---
+++ Nogareru3D/Assets/Scripts/InputNomeJogador.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputNomeJogador : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_InputField inputNomeJogador = null;
+    [SerializeField] private Button botaoContinuar = null;
+
+    public static string NomeDisplay { get; private set; }
+
+    private const string PlayerPrefsNameKey = "PlayerName"; // Chave como se fosse de um dicionario
+
+    void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        if(!PlayerPrefs.HasKey(PlayerPrefsNameKey)) // Se essa chave existir
+        {
+            return;
+        }
+
+        string nomePadrao = PlayerPrefs.GetString(PlayerPrefsNameKey); // Seta o nome la do input
+
+        inputNomeJogador.text = nomePadrao;
+
+        SetPlayerName(nomePadrao);
+    }
+
+    public void SetPlayerName(string nome)
+    {
+        botaoContinuar.interactable = !string.IsNullOrEmpty(nome);
+    }
+
+    public void SavePlayerName()
+    {
+        NomeDisplay = inputNomeJogador.text;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey,NomeDisplay);
+    }
+}
